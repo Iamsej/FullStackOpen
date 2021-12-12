c683afea446e7202e8b9439fa63955c0eb7187d2
Iamsej <iamsamueljones@gmail.com>
2021-12-12 01:16:46 -0500

    2.7 functional no warnings

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,24 +1,68 @@
 import React, { useState } from 'react'
 
+const Person = ({name}) => {
+  return(
+    <>
+      <p>
+        {name}
+      </p>
+    </>
+  )
+}
+
+
+const Numbers = ({persons}) => {
+  console.log(persons)
+  const personsmap = persons.map(person => 
+  <Person key={person.name} name={person.name} />)
+  return(
+    <>
+      {personsmap}
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleInput = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const address = {
+      name: newName
+    }
+  
+    persons.every(personEqualityChecker)
+    ? setPersons(persons.concat(address))
+    : alert(`${newName} is already in the Phonebook`)
+
+    
+    setNewName('')
+  }
+
+  const personEqualityChecker = (person) => 
+    person.name !== newName
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInput}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Numbers persons={persons}/>
     </div>
   )
 }
