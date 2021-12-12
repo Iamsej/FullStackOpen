a3a185a079a8c0a8e54164adf6cfe1f4cc95d8b4
Iamsej <iamsamueljones@gmail.com>
2021-12-12 17:40:34 -0500

    2.9 functional, no warnings

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const Person = ({name}) => {
+const Person = ({name, number}) => {
   return(
     <>
       <p>
-        {name}
+        {name}: {number}
       </p>
     </>
   )
@@ -12,9 +12,8 @@ const Person = ({name}) => {
 
 
 const Numbers = ({persons}) => {
-  console.log(persons)
   const personsmap = persons.map(person => 
-  <Person key={person.name} name={person.name} />)
+  <Person key={person.name} name={person.name} number={person.number} />)
   return(
     <>
       {personsmap}
@@ -24,45 +23,71 @@ const Numbers = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
 
-  const handleInput = (event) => {
+  const handleNameInput = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addName = (event) => {
     event.preventDefault()
     const address = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
   
-    persons.every(personEqualityChecker)
+    persons.every((person) => 
+    person.name !== newName)
     ? setPersons(persons.concat(address))
     : alert(`${newName} is already in the Phonebook`)
-
-    
+ 
     setNewName('')
+    setNewNumber('')
   }
 
-  const personEqualityChecker = (person) => 
-    person.name !== newName
-  
+  const searchBar = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const searchFilter = (person) => {
+    if (person.name.toLowerCase().indexOf(searchName.toLowerCase()) >-1) {
+      return(
+        person
+      )
+    }
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>Search for name: <input value={searchName} onChange={searchBar}/></p> 
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleInput}/>
+          name: <input value={newName} onChange={handleNameInput}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInput}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <Numbers persons={(persons.map(searchFilter)).filter(e => e !== undefined)}/>
     </div>
   )
 }
