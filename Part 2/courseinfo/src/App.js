06b57420cd4c9ea651e2f39965454352bb4a62ea
Iamsej <iamsamueljones@gmail.com>
2021-12-11 19:33:16 -0500

    2.4 functional, no warnings

--- Part 2/courseinfo/src/App.js
+++ Part 2/courseinfo/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({course}) => {
   return (
     <>
-      <h1>{course}</h1>
+      <h2>{course}</h2>
     </>
   );
 };
@@ -36,7 +36,7 @@ const Total = ({parts}) => {
   return (
     <>
       <p>
-        Number of exercises{' '}  
+        Total exercises in this course{' '}  
         {exercisecount.reduce((a, b) => a + b)}
       </p>
     </>
@@ -58,30 +58,58 @@ const Course = ({course}) => {
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+const coursemap = courses.map(course => <Course key={course.id} course={course} />)
 
   return (
-    <Course course={course} />
+    <>
+      <h1>Web Development Curriculum</h1>
+      {coursemap}
+    </>
   );
 };
 
