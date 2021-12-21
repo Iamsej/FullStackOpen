9167bb684b950a1114311e68974b6119848fc2b6
Iamsej <iamsamueljones@gmail.com>
2021-12-21 12:39:15 -0500

    2.20 functional, no warnings

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -5,12 +5,32 @@ import SubmissionForm from './components/SubmissionForm'
 import serviceRequest from './services/persons'
 
 
+const Notification = ({ message, error }) => {
+  if (message === null && error === null) {
+    return null
+  } else if (message === null) {
+
+    return (
+      <div className='error'>
+        {error}
+      </div>
+    )
+  }
+
+  return(
+    <div className='message'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+  const [message, setMessage] = useState(null)
+  const [error, setError] = useState(null)
 
   const findIdForName= (targetName) => {
     const personObject = persons.find(person => person.name === targetName)
@@ -45,15 +65,26 @@ const App = () => {
     ? serviceRequest
         .sendName(address)
         .then(response => {setPersons(persons.concat(response))})
+        .then(() => {setMessage(`${newName} sucessfully added`)})
+          .then(()=> setTimeout(()=> {setMessage(null)}, 5000))
     : window.confirm(
       `${newName} is already in the Phonebook. Would you like to update their number?`)
       ? serviceRequest
           .updateName(findIdForName(address.name), address)
           .then(returnedPerson => {
             setPersons(persons.map(person => person.name !== returnedPerson.name ? person : returnedPerson))})
+          .then(() => {setMessage(`${address.name} sucessfully updated`)})
+          .then(()=> {setTimeout(()=> {setMessage(null)}, 5000)})
+          .catch(error => {
+            setError(`${newName} already removed from phonebook`);
+            setTimeout(() => {
+              setError(null)
+            }, 5000);
+            setPersons(persons.filter(person => person.name !== newName));
+          })
       : alert('Cancelled update')
  
-    setNewName('')
+  setNewName('')
     setNewNumber('')
   }
 
@@ -64,6 +95,8 @@ const App = () => {
     ? serviceRequest
       .deleteName(deletionCandidate)
       .then( () => {setPersons(persons.filter(person => person.id !== deletionCandidate))})
+      .then(() => {setMessage(`${deletionName} sucessfully deleted`)})
+          .then(()=> setTimeout(()=> {setMessage(null)}, 5000))
     : alert('Deletion cancelled')
   }
 
@@ -74,6 +107,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={message} error={error}/>
       <h2>Phonebook</h2>
       <Filter value={searchName} function={searchBar}/>
       <h2>Add a new contact</h2>
